894fb3db82d1c45b9df77d175d3dad1ebc78ebc4
ecj4real <ecj4real@gmail.com>
2017-08-07 15:46:34 +0100

    Initial State of Project

---
+++ laravel/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MyFirstLaravel\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'MyFirstLaravel\Events\Event' => [
+            'MyFirstLaravel\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
